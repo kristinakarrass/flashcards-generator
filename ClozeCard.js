52176979850c23ccf245da56aeaa512f8a12a6aa
Kristina Karrass <kristinakarrass@gmail.com>
2017-04-27 12:36:18 -0400

    cloze cards

--- ClozeCard.js
+++ ClozeCard.js
@@ -7,6 +7,11 @@ var ClozeCard = function(text, cloze) {
 	this.text = text;
 	this.cloze = cloze;
 
+	this.addClozeCard = function(t, c) {
+		this.ClozeCardsArray.push(new ClozeCard);
+	}
+	var f = text;
+	var b = cloze;
 	this.addBasicCard = function(f, b) {
 		this.BasicCardsArary.push(new BasicCard(f, b));
 	}
