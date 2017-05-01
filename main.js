a06882c0468207c7888b5ea5a6f6c39659433c59
Kristina Karrass <kristinakarrass@gmail.com>
2017-05-01 11:56:23 -0400

    cloze working

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//stringify answer!!!
+
 var BasicCard = require("./BasicCards.js");
 var ClozeCard = require("./ClozeCard");
 var inquirer = require("inquirer");
@@ -8,43 +10,48 @@ var clozeDeck = [];
 
 function makeBasicCard() {
 
-	inquirer.prompt([
-	{
-		name: "front",
-		message: "Please enter the question for the front side of your flashcard."
-	},
-	{
-		name: "back",
-		message: "Please enter the answer for the back side of your flashcard."
-	}
-	]).then(function(answer) {
-		var newBasicCard = new BasicCard(answer.front, answer.back);
-		// console.log(newBasicCard);
+    inquirer.prompt([{
+        type: "input",
+        name: "front",
+        message: "Please enter the question for the front side of your flashcard."
+    }, {
+        type: "input",
+        name: "back",
+        message: "Please enter the answer for the back side of your flashcard."
+    }]).then(function(answer) {
+        var newBasicCard = new BasicCard(answer.front, answer.back);
+        // console.log(newBasicCard);
         basicDeck.push(newBasicCard);
         console.log(basicDeck);
-        fs.appendFile("basicDeck.txt", newBasicCard,  function(err) {
-                        if (!err) {
-                            console.log("working");
-                        }
-                    });
-	})
+        fs.appendFile("basicDeck.txt", JSON.stringify(answer), function(err) {
+            if (!err) {
+                console.log("working");
+            }
+        });
+    })
 
 };
 
 function makeClozeCard() {
 
-	inquirer.prompt([
-	{
-		name: "text",
-		message: "Enter your text for the cloze card's front.",
-	},
-	{
-		name: "cloze",
-		message: "Enter the key word you would like to take out of the sentence."
-	}]).then(function(answer) {
-		var newClozeCard = new ClozeCard(answer.text, answer.cloze);
-		clozeDeck.push(newClozeCard);
-	})
-}
+    inquirer.prompt([{
+        type: "input",
+        name: "text",
+        message: "Enter your text for the cloze card's front.",
+    }, {
+        type: "input",
+        name: "cloze",
+        message: "Enter the key word you would like to take out of the sentence."
+    }]).then(function(answer) {
+    	console.log(answer);
+        var newClozeCard = new ClozeCard(answer.text, answer.cloze);
+        fs.appendFile("clozeDeck.txt", JSON.stringify(answer), function(err) {
+            if (!err) {
+                console.log("working");
+            }
+        });
+    })
+};
 
-makeBasicCard();
+// makeBasicCard();
+makeClozeCard();
